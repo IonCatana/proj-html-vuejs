8b2fb782676d3eb68b66fc3764c999fedc75ddac
IonCatana <catana.ion17@yahoo.it>
2022-03-21 20:55:36 +0100

    Componente StartShoping done and some fix

--- src/components/Main/SpecialMoments.vue
+++ src/components/Main/SpecialMoments.vue
@@ -7,9 +7,29 @@
       </div>
       <div class="cards">
         <div class="card">
+          <div class="info text-center">
+            <div class="hover_text">
+              <div>
+                <p class="manatee">Internazional Bakery</p>
+                <h1>Corporate Events</h1>
+                <button class="btn">Explore More</button>
+              </div>
+            </div>
+            <img src="../../assets/img/corporate-hover-bg.jpg" alt="" />
+          </div>
           <img src="../../assets/img/corporate-bg.jpg" alt="" />
         </div>
         <div class="card">
+          <div class="info text-center">
+            <div class="hover_text">
+              <div>
+                <p class="manatee">Private Dining Hall</p>
+                <h1>Weddings&Parties</h1>
+                <button class="btn">Explore More</button>
+              </div>
+            </div>
+            <img src="../../assets/img/wedding-hover-bg.jpg" alt="" />
+          </div>
           <img src="../../assets/img/wedding-bg.jpg" alt="" />
         </div>
       </div>
@@ -48,9 +68,47 @@ export default {
   .card {
     width: 50%;
     object-fit: contain;
+    position: relative;
   }
 }
 img {
   width: 100%;
+}
+.info {
+  visibility: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  color: #ffffff;
+  overflow: auto;
+  background-color: rgba($color: #000000, $alpha: 0.2);
+}
+.hover_text {  
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  p{
+    text-transform: uppercase;
+    font-size: 10px;    
+  }
+  h1{
+    font-size: 32px;
+    font-weight: 600;
+    padding-bottom: 10px;
+  }
+  .btn{
+    background-color: #5b2ca2;
+    color: #ffffff;
+    padding: 8px 20px;
+  }
+  
+}
+.card:hover .info{
+  visibility: visible;
+  cursor: pointer;  
 }
 </style>
--- src/components/Main/StartShoping.vue
+++ src/components/Main/StartShoping.vue
@@ -2,7 +2,7 @@
   <div id="start_shoping">
     <div class="container">
       <div class="row">
-        <div class="col-4">
+        <div class="col-4 ">
           <span class="manatee">Our Products</span>
           <p class="daisy_bush">
             All our delectable pastries are backed fresh in our Kitchen very
@@ -14,6 +14,13 @@
           <div class="row">
             <div class="col-6">
               <i class="fa-solid fa-chevron-left"></i>
+              <div class="info text-center">
+                <div>
+                  <h2>Choco Chips Cookies</h2>
+                  <p>Coockies, Pastries</p>
+                  <span>$19.00 $39.00</span>
+                </div>
+              </div>
               <img
                 src="../../assets/img/choco-chip-cookies-400x510.jpg"
                 alt=""
@@ -21,6 +28,13 @@
             </div>
             <div class="col-6">
               <i class="fa-solid fa-chevron-right"></i>
+              <div class="info text-center">
+                <div>
+                  <h2>Strawberry Jam Cookies</h2>
+                  <p>Coockies, Pastries</p>
+                  <span>$24.00 $62.00</span>
+                </div>
+              </div>
               <img
                 src="../../assets/img/strawberry-jam-cookies-400x510.jpg"
                 alt=""
@@ -47,6 +61,8 @@ export default {
 }
 
 .col-4 {
+  padding: 0;
+  padding-right: 50px;
   span {
     font-size: 10px;
     text-transform: uppercase;
@@ -70,18 +86,24 @@ export default {
 .col-6 {
   position: relative;
   width: 370px;
+  padding: 0;
   img {
     width: 100%;
   }
+}
+.col-6:hover .info {
+  visibility: visible;  
+  cursor: pointer;
 }
 .row {
   display: flex;
   justify-content: space-between;
 }
 .fa-chevron-left {
+  z-index: 9;
   position: absolute;
   top: 50%;
-  left: 21px;
+  left: 9px;
   transform: translate(-50%, -50%);
   background-color: #bda2f3;
   color: #ffffff;
@@ -89,13 +111,37 @@ export default {
   padding: 15px 6px;
 }
 .fa-chevron-right {
+  z-index: 9;
   position: absolute;
   top: 50%;
-  right: 3px;
+  right: -9px;
   transform: translate(-50%, -50%);
   background-color: #bda2f3;
   color: #ffffff;
   font-size: 9px;
   padding: 15px 6px;
+}
+.info {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  visibility: hidden;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  color: #ffffff;
+  overflow: auto;
+  background-color: rgba($color: #000000, $alpha: 0.2);
+}
+h2 {
+  font-size: 21px;
+  font-weight: 600;
+}
+p {
+  font-size: 13px;
+}
+span {
+  font-size: 20px;
+  font-weight: 600;
 }
 </style>
