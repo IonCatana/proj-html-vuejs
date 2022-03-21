735ed9481d4c040e67d1b211b32126c347cc887a
IonCatana <catana.ion17@yahoo.it>
2022-03-21 19:06:12 +0100

    add component SpecialMoments

--- src/components/Main/MyMain.vue
+++ src/components/Main/MyMain.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="my_main">
-    <StartShoping />    
+    <StartShoping />
+    <SpecialMoments />
   </div>
 </template>
 
 <script>
-import StartShoping from "./StartShoping.vue"
-  export default {
-    name: 'MyMain',
-    components: {
-      StartShoping,
-    }
-  }
+import StartShoping from "./StartShoping.vue";
+import SpecialMoments from "./SpecialMoments.vue";
+export default {
+  name: "MyMain",
+  components: {
+    StartShoping,
+    SpecialMoments,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
