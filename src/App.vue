13448c43a8135e5e46b6e6a638f03b62aecf2de6
IonCatana <catana.ion17@yahoo.it>
2022-03-21 12:40:09 +0100

    add img, scss components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">   
+    <HeaderNavBar />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderNavBar from './components/HeaderNavBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderNavBar,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import '../node_modules/bootstrap/scss/bootstrap.scss';
+@import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
+@import './assets/scss/variables.scss';
+@import './assets/scss/common.scss';
 </style>
---
+++ src/components/HeaderNavBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="header_nav_bar">
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HeaderNavBar'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
